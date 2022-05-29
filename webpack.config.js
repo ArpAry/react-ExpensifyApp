fa7d02fafb3f64b9f15d1e7ddb75d53973a3f0f6
ArpAry <arpitarya3299@gmail.com>
2022-05-29 20:35:09 +0530

    delelting bundle js and setup is for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,35 @@
 const path = require("path");
-module.exports = { presets: "@babel/preset-env" };
-module.exports = {
-  mode: "development",
-  entry: "./src/app.js",
 
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
-  },
 
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s?css$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-    ],
-  },
-
-  devtool: "eval-cheap-module-source-map",
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
+module.exports=(env) => {
+  console.log("env",env);
+  return {
+    mode: "development",
+    entry: "./src/app.js",
+    presets: "@babel/preset-env",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
     },
-  },
-};
+  
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+      ],
+    },
+  
+    devtool: "eval-cheap-module-source-map",
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "public"),
+      },
+    },
+  }};
