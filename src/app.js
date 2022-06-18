24b85f038ceee4a0dc38f3957c36e3ede94368aa
ArpAry <arpitarya3299@gmail.com>
2022-06-18 19:04:03 +0530

    firebase  events and promise is going to complete

--- src/app.js
+++ src/app.js
@@ -11,19 +11,11 @@ import getVisibleExpenses from "./selectors/expenses";
 //import expenses from "./reducers/expenses";
 import './firebase/firebase'
 const store = configureStore();
-import "./playground/promises";
-//require("./firebase/firebase");
-// import "D:\React-course-projects\Expensify-App\src\firebase\firebase.js";
-//console.log(dispatcher);
-// store.dispatch(addExpense({ description: "water bill", amount: 15000 }));
-// store.dispatch(addExpense({ description: "gas bill", amount: 2010 ,createdAt:105}));
-// //store.dispatch(addExpense({ description: "hand bill", amount: 2010 ,createdAt:105}));
-// store.dispatch(addExpense({ description:"rent  bill", amount:1005, createdAt:102}));
-// //store.dispatch(addExpense({ description:"hello", amount:1005, createdAt:102}));
+//import "./playground/promises";
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expense, state.filters);
-console.log(visibleExpenses);
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses(state.expense, state.filters);
+//console.log(visibleExpenses);
 
 const jsx = (
  
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,7 @@
-
-import { getAnalytics } from "firebase/analytics";
+// Connect to Firebase v9 - modular
 import {initializeApp} from 'firebase/app'
-import {getDatabase, ref, set} from 'firebase/database'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {getDatabase, ref,get,query,child,update,remove, set,onValue} from 'firebase/database'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDjHgo6-9N-JPSb0jpeDDRcu6ZclJDgPx8",
   authDomain: "expensifyappp.firebaseapp.com",
@@ -18,19 +13,69 @@ const firebaseConfig = {
   measurementId: "G-E2YNWT1X8B"
 };
 
-// Initialize Firebase
+
+// Initialize Firebase v9
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-const db = getDatabase()
+const db = getDatabase(app);
+const dbRef = ref(db);
+//const age=ref(db,"age");
+//const database=firebase.database();
 
-set(ref(db), {name: 'Arpit',
-age:21,
-location:{
-  city:"Ghaziabad",
-  country:"India"
-}
-});
 
-//set(ref(db),("attributes"),{height :5.5,weight:70});
-//set(ref(db).child('age').set(30));
+
+
+
+
+
+// set(dbRef, {name: 'Arpit',
+// age:21,
+// job:{
+// company:"amazon",
+// title:"SDE"
+// },
+// location:{
+//   city:"Ghaziabad",
+//   country:"India"
+// }
+// }).then(()=>{
+//   console.log("Datat  is Saved");
+// }).catch((e)=>{
+//   console.log("This is failed ",e);
+// });
+
+
+//child_remove_Event
+//child_Changed_Event
+//child_Added_Event
+
+
+
+//*****************/
+//removing data  from database
+//****************/
+
+// remove(child(dbRef, ("name")))
+//   .then(() => {
+//     console.log('I fetched something ...');
+//   })
+//   .catch((e) => {
+//   console.log('Data fetch failed', e);
+//   });
+
+//*****************/
+//updating data in the  database
+//****************/
+
+//update(child(age,set(28)));
+
+//*****************/
+//fetching database
+//****************/
+
+// onValue(dbRef,(dataSnapshot)=>{
+//   const val=dataSnapshot.val();
+//   console.log(`${val.name} is  a  ${val.job.title} at ${val.job.company}`)
+// })
+
+// dbRef.on('value',(snapshot)=>{console.log(snapshot.val())});
