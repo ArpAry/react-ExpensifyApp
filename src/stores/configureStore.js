e9be7446f511bb77cba0ac18069cc06b2df2582f
ArpAry <arpitarya3299@gmail.com>
2022-06-19 23:20:12 +0530

    working with thunk

--- src/stores/configureStore.js
+++ src/stores/configureStore.js
@@ -1,16 +1,21 @@
-import {createStore,combineReducers} from 'redux';
+import {createStore,combineReducers,Middleware, applyMiddleware, compose} from 'redux';
 import expensifyReducer from '../reducers/expenses';
 import filterReducer from '../reducers/filter';
+import thunk from 'redux-thunk';
 //
 //Store Creation and Combine Reducer
 //
+
+const composeEnhancer=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||compose;
 export default ()=>{
     const store = createStore(
         combineReducers({
           expense: expensifyReducer,
           filters: filterReducer,
         }),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        //due to the below statement we loose functionality from developer tool we will use it using making const
+      composeEnhancer (applyMiddleware(thunk))
+     //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
       );
       return store;
 
